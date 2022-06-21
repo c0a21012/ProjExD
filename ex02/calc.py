bb308c1bf2cbb7e84c0683df78df1d070e96456c
ishikawa <c0a21012da@edu.teu.ac.jp>
2022-06-21 15:27:49 +0900

    問題7

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,8 +5,15 @@ import tkinter.messagebox as tkm
 def button_click(event):
     btn = event.widget
     n = btn["text"]
+    if n == "=":
+        eq = entry.get()
+        res = eval(eq)
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, res)
+
     #tkm.showinfo("",f"{n}ボタン押された")
     entry.insert(tk.END, n)
+
 if __name__ == "__main__":
     root = tk.Tk()
     #root.geometry("300x500")
@@ -15,17 +22,17 @@ if __name__ == "__main__":
     entry = tk.Entry(root, 
                     justify="right", 
                     width=10, 
-                    font=("Times New Roman",40)
+                    font=("Times New Roman", 40)
                     )
     entry.grid(row=0, column=0, columnspan=3)
 
     r, c = 2, 0
-    for i, n in enumerate([i for i in range(9, -1, -1)] + ["+"]):
+    for i, n in enumerate([i for i in range(9, -1, -1)] + ["+", "="]):
         btn = tk.Button(root, 
                         text=f"{n}", 
                         width=4, 
                         height=2,
-                        font=("Times New Roman",30)
+                        font=("Times New Roman", 30)
                         )
         btn.bind("<1>", button_click)
         btn.grid(row=r, column=c)
