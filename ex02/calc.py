1e14a27475de937127447bdabf134186ba06bdfb
ishikawa <c0a21012da@edu.teu.ac.jp>
2022-06-21 16:31:40 +0900

    追加2

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,6 +1,7 @@
 from calendar import c
 import tkinter as tk
 import tkinter.messagebox as tkm
+import math
 
 def button_click(event):
     btn = event.widget
@@ -10,6 +11,24 @@ def button_click(event):
         res = eval(eq)
         entry.delete(0, tk.END)
         entry.insert(tk.END, res)
+    elif n == "sin":
+        get = entry.get()
+        ans = eval(get)
+        sin = math.sin(math.radians(ans))
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, sin)
+    elif n == "cos":
+        get = entry.get()
+        ans = eval(get)
+        cos = math.cos(math.radians(ans))
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, cos)
+    elif n == "tan":
+        get = entry.get()
+        ans = eval(get)
+        tan = math.tan(math.radians(ans))
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, tan)
     else:
         entry.insert(tk.END, n)
 
@@ -28,7 +47,7 @@ if __name__ == "__main__":
     entry.grid(row=0, column=0, columnspan=3)
 
     r, c = 2, 0
-    for i, n in enumerate([i for i in range(9, -1, -1)] + ["+","-", "*", "/", "="]):
+    for i, n in enumerate([i for i in range(9, -1, -1)] + ["+","-", "*", "/", "sin","cos", "tan", "="]):
         btn = tk.Button(root, 
                         text=f"{n}", 
                         width=4, 
@@ -38,7 +57,7 @@ if __name__ == "__main__":
         btn.bind("<1>", button_click)
         btn.grid(row=r, column=c)
         c += 1
-        if (i+1) % 3 == 0:
+        if (i+1) % 5 == 0:
             r += 1
             c = 0
         
