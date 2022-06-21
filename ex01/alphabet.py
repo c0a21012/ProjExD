0ef1446cbf246a2152abbd74d9bd3fc23baa8b0a
ishikawa <c0a21012da@edu.teu.ac.jp>
2022-06-21 14:37:29 +0900

    問題3

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -1,44 +1,44 @@
 import random
+import datetime
 
-taisyou = 10
-kesson = 2
-max = 2
+taisyou = 10    #対象文字の数
+kesson = 2      #欠損文字の数
+max = 2         #最大繰り返し回数
 
 def main():
-    seikai = shutsudai()
+    seikai = set(shutsudai())
     for i in range(max):
         kaito()
-        if set(seikai) == set(kaito()):
+        if seikai == kaito():
             print("正解")
             break
-        elif set(seikai) != set(kaito()):
+        elif seikai != kaito():
             print("もう一度")
-        elif i == max-1 and set(seikai) != set(kaito()):
+        elif i == max-1 and seikai != kaito():
             print("残念でした")
 
 def shutsudai():
     alphabet = [chr(i + 65) for i in range(26)]
     random_alphabet = random.sample(alphabet, taisyou)
     print(f"対象文字は{random_alphabet}")
-    hyouzi = random.sample(random_alphabet, taisyou-kesson)
+    hyouzi = random.sample(random_alphabet, taisyou-kesson)         #表示文字
     print(f"表示文字は{hyouzi}")
 
     kessonmoji = []
     for j in random_alphabet:
         if j not in hyouzi:
-            kesson.append(j)
+            kessonmoji.append(str(j))
 
     return kessonmoji 
 
 def kaito():
     kotae = []
     ans = input("欠損文字は何個?")
-    if ans != kesson:
+    if int(ans) != kesson:
         print("不正解")
-        return 0
     else:
         print("正解。具体的に何が欠損したか一文字ずつ入力せよ")
         for i in range(kesson):
-            a = input(f"{i}個目の欠損文字")
-            kotae.appned(a)
-    return kotae        
+            a = input(f"{i+1}個目の欠損文字")
+            kotae.append(a)
+    return set(kotae)
---
+++ ex02/calc.py
@@ -0,0 +1,29 @@
+from calendar import c
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+def button_click(event):
+    btn = event.widget
+    n = btn["text"]
+    tkm.showinfo("",f"{n}ボタン押された")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.geometry("300x500")
+    root.title("電卓")
+    r, c = 0, 0
+    for n in range(9, -1, -1):
+        btn = tk.Button(root, 
+                        text=f"{n}", 
+                        width=4, 
+                        height=2,
+                        font=("Times New Roman",30)
+                        )
+        btn.bind("<1>", button_click)
+        btn.grid(row=r, column=c)
+        c += 1
+        if (n+2) % 3 == 0:
+            r += 1
+            c = 0
+        
+    root.mainloop()
