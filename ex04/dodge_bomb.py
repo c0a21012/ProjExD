addaddba41d343283eae1d7cb2f0c89202799016
ishikawa <c0a21012da@edu.teu.ac.jp>
2022-07-05 14:34:15 +0900

    練習4

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -8,8 +8,6 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen_sfc = pg.display.set_mode((1600, 900))
     screen_rect = screen_sfc.get_rect()
-
-    #練習2
     pg.image.load("fig/pg_bg.jpg")
     bgimg_sfc = pg.image.load("fig/pg_bg.jpg")
     bgimg_rect = bgimg_sfc.get_rect()
@@ -24,11 +22,24 @@ def main():
     
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rect)
+        
 
+        #練習2
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-        
+        #練習4
+        key_lst = pg.key.get_pressed()
+        if key_lst [pg.K_UP] == True:#y座標を-1
+            kkimg_rect.centery -= 1
+        if key_lst [pg.K_DOWN] == True:#y座標を+1
+            kkimg_rect.centery += 1
+        if key_lst [pg.K_LEFT] == True:#x座標を-1
+            kkimg_rect.centerx -= 1
+        if key_lst [pg.K_RIGHT] == True:#x座標を+1
+            kkimg_rect.centerx += 1
+        screen_sfc.blit(kkimg_sfc, kkimg_rect)
+
         pg.display.update()
         clock.tick(1000)
 
