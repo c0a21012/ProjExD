dab75270ace80ca846fea0d5edc8ccb632d76444
ishikawa <c0a21012da@edu.teu.ac.jp>
2022-07-05 15:00:09 +0900

    練習5

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,10 +1,11 @@
 import pygame as pg
 import sys
+import random
 
 def main():
     clock  = pg.time.Clock()
 
-    #練習１
+    #練習１：スクリーンと背景
     pg.display.set_caption("逃げろ！こうかとん")
     screen_sfc = pg.display.set_mode((1600, 900))
     screen_rect = screen_sfc.get_rect()
@@ -13,11 +14,19 @@ def main():
     bgimg_rect = bgimg_sfc.get_rect()
     screen_sfc.blit(bgimg_sfc, bgimg_rect)
 
-    #練習3
+    #練習3：こうかとん
     kkimg_sfc = pg.image.load("fig/6.png")
     kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)
     kkimg_rect = kkimg_sfc.get_rect()
     kkimg_rect.center = 900, 400
+
+    #練習5：爆弾
+    bombimg_sfc = pg.Surface((20, 20)) #Surface
+    pg.draw.circle(bombimg_sfc, (255, 0, 0), (10, 10), 10)
+    bombimg_rect = bombimg_sfc.get_rect()
+    bombimg_rect.centerx = random.randint(0, screen_rect.width)
+    bombimg_rect.centery = random.randint(0, screen_rect.height)
+    vx, vy = +1, +1 #練習6
 
     
     while True:
@@ -39,6 +48,12 @@ def main():
         if key_lst [pg.K_RIGHT] == True:#x座標を+1
             kkimg_rect.centerx += 1
         screen_sfc.blit(kkimg_sfc, kkimg_rect)
+
+        #練習5
+        screen_sfc.blit(bombimg_sfc, bombimg_rect)
+
+        #練習6
+        bombimg_rect.move_ip(vx, vy)
 
         pg.display.update()
         clock.tick(1000)
