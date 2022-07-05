fa118d90afadf52b89e76195312204a5e2c2af7a
ishikawa <c0a21012da@edu.teu.ac.jp>
2022-07-05 14:24:06 +0900

    練習1,2

---
+++ ex04/dodge_bomb.py
@@ -0,0 +1,29 @@
+import pygame as pg
+import sys
+
+def main():
+    clock  = pg.time.Clock()
+    pg.display.set_caption("逃げろ！こうかとん")
+    screen_sfc = pg.display.set_mode((1600, 900))
+    screen_rect = screen_sfc.get_rect()
+
+    pg.image.load("fig/pg_bg.jpg")
+    bgimg_sfc = pg.image.load("fig/pg_bg.jpg")
+    bgimg_rect = bgimg_sfc.get_rect()
+    screen_sfc.blit(bgimg_sfc, bgimg_rect)
+    
+    while True:
+        screen_sfc.blit(bgimg_sfc, bgimg_rect)
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+        
+        pg.display.update()
+        clock.tick(1000)
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
