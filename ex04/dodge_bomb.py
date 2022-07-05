94f13b8933e686a407fc050fca892b0887aea239
ishikawa <c0a21012da@edu.teu.ac.jp>
2022-07-05 14:24:41 +0900

    練習3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -3,14 +3,24 @@ import sys
 
 def main():
     clock  = pg.time.Clock()
+
+    #練習１
     pg.display.set_caption("逃げろ！こうかとん")
     screen_sfc = pg.display.set_mode((1600, 900))
     screen_rect = screen_sfc.get_rect()
 
+    #練習2
     pg.image.load("fig/pg_bg.jpg")
     bgimg_sfc = pg.image.load("fig/pg_bg.jpg")
     bgimg_rect = bgimg_sfc.get_rect()
     screen_sfc.blit(bgimg_sfc, bgimg_rect)
+
+    #練習3
+    kkimg_sfc = pg.image.load("fig/6.png")
+    kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)
+    kkimg_rect = kkimg_sfc.get_rect()
+    kkimg_rect.center = 900, 400
+
     
     while True:
         screen_sfc.blit(bgimg_sfc, bgimg_rect)
