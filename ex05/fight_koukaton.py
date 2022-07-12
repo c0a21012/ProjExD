2a840c8ad3a9aaf1487174b918033adabfe1343f
ishikawa <c0a21012da@edu.teu.ac.jp>
2022-07-12 17:58:48 +0900

    20に対する修正

--- ex05/fight_koukaton.py
+++ ex05/fight_koukaton.py
@@ -1,15 +1,9 @@
-from pickle import NONE
-from zipfile import ZIP_MAX_COMMENT
 import pygame as pg
 import sys
 import random
 
 
 def check_bound(rct, scr_rct):
-    '''
-    [1] rct: こうかとん or 爆弾のRect
-    [2] scr_rct: スクリーンのRect
-    '''
     yoko, tate = +1, +1 # 領域内
     if rct.left < scr_rct.left or scr_rct.right  < rct.right : 
         yoko = -1 # 領域外
@@ -64,8 +58,6 @@ class Bird:
         self.blit(scr)
 
 
-
-
 class Bomb:
     def __init__(self, color, zoom, vxy, scr):
         self.sfc = pg.Surface((2*zoom, 2*zoom)) # Surface
@@ -78,17 +70,14 @@ class Bomb:
 
     def update(self, scr):
         self.rct.move_ip(self.vx, self.vy)
-        
-        # 練習7
         yoko, tate = check_bound(self.rct, scr.rct)
         self.vx *= yoko
         self.vy *= tate
-
         self.blit(scr)
 
     def blit(self, scr):
         scr.sfc.blit(self.sfc, self.rct)
-        # 練習5
+
         
 class BirdBomb:
     def __init__(self, color, zoom, xy):
@@ -97,12 +86,8 @@ class BirdBomb:
         pg.draw.circle(self.sfc, color, (zoom, zoom), zoom)
         self.rct = self.sfc.get_rect() # Rect
         self.rct.center = xy
-        #self.rct.centerx += 2
 
     def update(self, scr):
-        # key_states = pg.key.get_pressed()
-        # if key_states[pg.K_SPACE]:
-        #     self.rct.centerx += 2
         key_states = pg.key.get_pressed() # 辞書
         if key_states[pg.K_w]: 
             self.rct.centery -= 1      
@@ -158,27 +143,17 @@ def main():
     bird_bomb = BirdBomb((0, 0, 255), 20, (900, 400))
     while True:
         scr.blit()
-        
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-            #  if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
-                
-                # while (0 <bird_bomb.rct.centerx < 1600 
-                #         and 0 < bird_bomb.rct.centery < 900):
-                #         i = 0
-                #         bird_bomb = BirdBomb((0, 0, 255), 20+i, (900, 400))
         kkt.update(scr)
         bkd.update(scr)
         ene.update(scr)
         bird_bomb.update(scr)
-                
         if kkt.rct.colliderect(bkd.rct) or kkt.rct.colliderect(ene.rct):
             return
-
         if ene.rct.colliderect(bird_bomb.rct):
             return 
-
         pg.display.update()
         clock.tick(1000)
 
